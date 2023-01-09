c5f32a2cacbcb6b29e449368966687e106f81eaa
Ranjeet <ranjeet.ggi@gmail.com>
2023-01-09 19:03:20 +0530

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import '../styles/App.css';
-const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
-const Join = (props) =>{
-  return(
-    <div id ="join">
-      {/* Access prop 'words' and print it using .join like words.join(',')*/}
-      <p>{props.words.join(",")}</p>
-    </div>
-  );
-};
-const App = () => {
+import React, { useState } from "react";
+import "../styles/App.css";
 
+const App = () => {
+  const [state, setState] = useState(true)
+  const handleToggle = () => {
+       if(state === true) {
+            setState(false)
+       } else {
+        setState(true)
+       }
+  }
   return (
     <div id="main">
-      <Join words={arr} />
+      {state === true ? (
+        <div id="welcome-div">{<h1>Welcome to Newton School!!</h1>}</div>) : (
+        <div id="about-div">{<h1>Newton School is a neo-university providing highly
+           immersive tech learning to millions of students, to tap into new-age 
+           tech opportunities.</h1>}</div>
+        )}
+      <button id="toggle" onClick={handleToggle}>Toggle</button>
     </div>
   );
 };
-
 
 export default App;
